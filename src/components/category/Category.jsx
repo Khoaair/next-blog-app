e6d201d784a79293e3f230aa3d325a9a795c2380
KhoaAir win <KhoaAir@gmail.com>
2023-10-25 23:28:36 +0700

    category

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,5 +1,91 @@
+import Link from 'next/link';
 import styles from './category.module.css';
+import Image from 'next/image';
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <div className={styles.categories}>
+      <h1 className={styles.categories__title}>Popular Categories</h1>
+      <div className={styles.categories__content}>
+        <Link
+          href='/blog?cat=style'
+          className={`${styles.content__category} ${styles.style}`}
+        >
+          <Image
+            src='/style.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Style
+        </Link>
+        <Link
+          href='/blog?cat=fashion'
+          className={`${styles.content__category} ${styles.fashion}`}
+        >
+          <Image
+            src='/fashion.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Fashion
+        </Link>
+        <Link
+          href='/blog?cat=food'
+          className={`${styles.content__category} ${styles.food}`}
+        >
+          <Image
+            src='/food.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Food
+        </Link>
+        <Link
+          href='/blog?cat=travel'
+          className={`${styles.content__category} ${styles.travel}`}
+        >
+          <Image
+            src='/travel.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Travel
+        </Link>
+        <Link
+          href='/blog?cat=culture'
+          className={`${styles.content__category} ${styles.culture}`}
+        >
+          <Image
+            src='/culture.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Culture
+        </Link>
+        <Link
+          href='/blog?cat=coding'
+          className={`${styles.content__category} ${styles.coding}`}
+        >
+          <Image
+            src='/coding.png'
+            alt=''
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Coding
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Category;
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,30 @@
+import Image from 'next/image';
 import styles from './featured.module.css';
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.featured}>
+      <h1 className={styles.featured__title}>
+        <b>Hey, John dev here!</b> Discover my stories and creawtive ideas.
+      </h1>
+      <div className={styles.featured__post}>
+        <div className={styles.featured__image}>
+          <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+        </div>
+        <div className={styles.featured__content}>
+          <h1 className={styles.content__title}>
+            Lorem ipsum dolor sit amet alim consectetur adipisicing elit.
+          </h1>
+          <p className={styles.content__desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Cupiditate, quam nisi magni ea laborum inventore voluptatum
+            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
+            quisquam! Harum unde sit culpa debitis.
+          </p>
+          <button className={styles.featured__button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Featured;
